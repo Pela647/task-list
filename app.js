4816214a11667d876002e302656e71d61ca4c4a6
Robel <robel@engineer.com>
2018-08-11 19:44:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //define UI variables
-const form = document.querySelector("#task-form");
-const taskList = document.querySelector(".collection");
-const clearBtn = document.querySelector(".clear-tasks");
-const filter = document.querySelector("#filter");
-const taskInput = document.querySelector("#task");
+const form = document.querySelector("#task-form"),
+      taskList = document.querySelector(".collection"),
+      clearBtn = document.querySelector(".clear-tasks"),
+      filter = document.querySelector("#filter"),
+      taskInput = document.querySelector("#task");
 
 // load all Event listeners
 
